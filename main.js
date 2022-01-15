8096fa895a51d55e00c4553f5feff467a72e1128
Abel Salau <62809777+abel-salau@users.noreply.github.com>
2022-01-14 19:00:22 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ $(document).ready(function() {
     $('.menu-toggler').on('click', function() {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
-    })
+    });
     
-
-    
-});
-
-$('.top-nav .nav-link').on('click', function() {
+    $('.top-nav .nav-link').on('click', function() {
     $('.menu-toggler').removeClass('open');
     $('.top-nav').removeClass('open');
    });
+
+   $('#up').on('click', function() {
+    $('html,body').animate({ scrollTop: 0 }, 3000);
+   });
+
+
+});
